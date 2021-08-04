0a425cddb75b412948d865f5a36deb99c1ef5b4f
JPatel87 <jaime_patel@hotmail.co.uk>
2021-08-04 14:11:16 +0000

    Edit index and css for choices section

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+//Check that the DOM is loaded before javascript functions are run
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.log('DOM Content fully loaded')
+});
+
+
