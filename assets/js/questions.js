d34221ee82737d7181cb4fb306643a0143d9f629
JPatel87 <jaime_patel@hotmail.co.uk>
2021-08-04 12:54:20 +0000

    Add questions for game into questions.js file

---
+++ assets/js/questions.js
@@ -0,0 +1,162 @@
+let questions = [
+    {
+        image: "assets/images/india.jpeg" alt="Taj Mahal, India"
+        a: "India",
+        b: "Cambodia",
+        c: "Pakistan",
+        d: "China",
+        answer: "option-a"
+    },
+    {
+        image: "assets/images/america.jpeg" alt="Statue of Liberty, America"
+        a: "Colombia",
+        b: "America",
+        c: "Cuba",
+        d: "Venezuala",
+        answer: "option-b"
+    },
+    {
+        image: "assets/images/austrailia.jpeg" alt="Twelve Apostles, Austrailia"
+        a: "Spain",
+        b: "Austrailia",
+        c: "Portugal",
+        d: "Greece",
+        answer: "option-b"
+    },
+    {
+        image: "assets/images/bavaria.jpeg" alt="Neuschwanstein, Bavaria"
+        a: "Holland",
+        b: "Belgium",
+        c: "Bavaria",
+        d: "Russia",
+        answer: "option-c"
+    },
+    {
+        image: "assets/images/brazil.jpeg" alt="Christ the Redeemer, Brazil"
+        a: "Spain",
+        b: "Brazil",
+        c: "Portugal",
+        d: "Greece",
+        answer: "option-b"
+    },
+    {
+        image: "assets/images/china.jpeg" alt="Great Wall of China, China"
+        a: "China",
+        b: "Japan",
+        c: "Sri Lanka",
+        d: "Malaysia",
+        answer: "option-a"
+    },
+    {
+        image: "assets/images/egypt.jpeg" alt="The Great Sphinx, Egypt"
+        a: "Dubai",
+        b: "Egypt",
+        c: "Kenya",
+        d: "South Africa",
+        answer: "option-b"
+    },
+    {
+        image: "assets/images/england.jpeg" alt="Stonehenge, England"
+        a: "Ireland",
+        b: "Italy",
+        c: "England",
+        d: "Norway",
+        answer: "option-c"
+    },
+    {
+        image: "assets/images/france.jpeg" alt="Eiffel Tower, France"
+        a: "Ireland",
+        b: "France",
+        c: "England",
+        d: "Norway",
+        answer: "option-b"
+    },
+    {
+        image: "assets/images/germany.jpeg" alt="Berlin Cathedral, Germany"
+        a: "Germany",
+        b: "Bulgaria",
+        c: "Switzerland",
+        d: "Swedan",
+        answer: "option-a"
+    },
+    {
+        image: "assets/images/greece.jpeg" alt="Blue domed church, Greece"
+        a: "Croatia",
+        b: "Malta",
+        c: "Bahamas",
+        d: "Greece",
+        answer: "option-d"
+    },
+    {
+        image: "assets/images/italy.jpeg" alt="Positano, Italy"
+        a: "Cyprus",
+        b: "Costa Rica",
+        c: "Brazil",
+        d: "Italy",
+        answer: "option-d"
+    },
+    {
+        image: "assets/images/japan.jpeg" alt="Mount Fuji, Japan"
+        a: "China",
+        b: "Philippenes",
+        c: "Morroco",
+        d: "Japan",
+        answer: "option-d"
+    },
+    {
+        image: "assets/images/malaysia.jpeg" alt="Petronas twin towers, Malaysia"
+        a: "Singapore",
+        b: "Malaysia",
+        c: "Kuwait",
+        d: "Dubai",
+        answer: "option-b"
+    },
+    {
+        image: "assets/images/mexico.jpeg" alt="Chichen Itza, Mexico"
+        a: "Italy",
+        b: "Kazakhstan",
+        c: "Mexico",
+        d: "Egypt",
+        answer: "option-c"
+    },
+    {
+        image: "assets/images/peru.jpeg" alt="Machu Pichu, Peru"
+        a: "Indonesia",
+        b: "Jordan",
+        c: "Peru",
+        d: "Argentina",
+        answer: "option-c"
+    },
+    {
+        image: "assets/images/russia.jpeg" alt="St Basil's Cathedral, Russia"
+        a: "America",
+        b: "Bangladesh",
+        c: "Russia",
+        d: "Botswana",
+        answer: "option-c"
+    },
+    {
+        image: "assets/images/south-africa.jpeg" alt="Table Mountain, South Africa"
+        a: "South Africa",
+        b: "Italy",
+        c: "Switzerland",
+        d: "Seychells",
+        answer: "option-a"
+    },
+    {
+        image: "assets/images/turkey.jpeg" alt="Hagia Sophia, Turkey"
+        a: "Israel",
+        b: "Turkey",
+        c: "Kuwait",
+        d: "Armenia",
+        answer: "option-a"
+    },
+    {
+        image: "assets/images/cambodia.jpeg" alt="Angkor Watt, Cambodia"
+        a: "China",
+        b: "Thailand",
+        c: "Malaysia",
+        d: "Cambodia",
+        answer: "option-d"
+    }
+];
